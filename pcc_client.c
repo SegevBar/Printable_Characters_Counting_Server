e28278f618d58d71224960d8f8e94b7268237ddd
SegevBar <segevbar94@gmail.com>
2023-01-23 16:07:00 +0200

    code compiles

---
+++ pcc_client.c
@@ -0,0 +1,155 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#define MAX_BATCH_SIZE 1048576
+#define min(X, Y) (((X) < (Y)) ? (X) : (Y))
+
+/* prototypes */
+int send_data_to_server(int sockfd, char* buffer, int count);
+int read_data_from_server(int sockfd, char* buffer, int count);
+
+
+int main(int argc, char const *argv[]) {
+    const char* IP_address;
+    int server_port;
+    const char* file_path;
+    FILE* fd;
+    struct sockaddr_in serv_addr; 
+    uint32_t N;
+    uint32_t endian_N;
+    int batch_size;
+    int bytes_left;
+    char* batch_file_buffer;
+    uint32_t C;
+    uint32_t endian_C;
+    char* recv_buff;
+    int sockfd = -1;
+
+    if (argc != 4) {
+        perror("ERROR: invalid number of arguments");
+        exit(1);
+    }
+    
+    /* get cmd arguments */
+    IP_address = argv[1];
+    server_port = atoi(argv[2]);
+    file_path = argv[3];
+
+    /* open given file */
+    fd = fopen(file_path, "r+");
+    if (fd == NULL) {
+        perror("ERROR: failed to open given file");
+        exit(1);
+    }
+    /* find given file total size 
+    https://www.tutorialspoint.com/c_standard_library/c_function_ftell.htm */
+    fseek(fd, 0L, SEEK_END);
+    N = ftell(fd);
+    fseek(fd, 0L, SEEK_SET);
+
+    /* create a TCP connection (code from rec10) */
+    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("ERROR: Could not create socket \n");
+        exit(1);
+    }
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(server_port); 
+    inet_pton(AF_INET, IP_address, &serv_addr.sin_addr.s_addr);  /*converting a string IP address to binary representation*/
+
+    /* connect */
+    if (connect(sockfd,
+              (struct sockaddr*) &serv_addr,
+               sizeof(serv_addr)) < 0) {
+        perror("ERROR: Connect Failed");
+        exit(1);
+    }
+
+    /* send N - given file size - to server */
+    endian_N = htonl(N);
+    send_data_to_server(sockfd, (char*) &endian_N, 4); /*32bits == 4bytes*/
+
+    /* send given file data in batches */
+    bytes_left = N;
+    while (bytes_left > 0) {
+        /* create a buffer of the maximal amount of data from given file */
+        batch_size = min(bytes_left, MAX_BATCH_SIZE);
+        
+        batch_file_buffer = (char*) malloc(batch_size*sizeof(char));
+        if (batch_file_buffer == NULL) {
+            perror("ERROR: an error accured while allocating memory to batch file buffer");
+            exit(1);
+        }
+        if (fread(batch_file_buffer, sizeof(char), batch_size, fd) < batch_size) {
+            perror("ERROR: an error accured while reading bytes from given file");
+            exit(1);
+        }
+
+        /* send current batch file buffer to server */
+        send_data_to_server(sockfd, batch_file_buffer, batch_size);
+        free(batch_file_buffer);
+
+        bytes_left -= batch_size;
+    }
+    fclose(fd);
+
+    /* get C from server */
+    recv_buff = (char*) &endian_C;
+    read_data_from_server(sockfd, recv_buff, 4);
+    C = ntohl(endian_C);
+    
+    printf("# of printable characters: %u\n", C);
+    
+    close(sockfd);
+    return 0;
+}
+
+/*
+* write to socket fd iterativly and returns the number of bytes written
+*
+*/
+int send_data_to_server(int sockfd, char* buffer, int count) {
+    int bytes_sent;
+    int totalsent = 0;
+
+    /* iterate until all buffer bytes are written to socket fd */
+    while (count > 0) {
+        bytes_sent = write(sockfd, buffer+totalsent, count);
+
+        if (bytes_sent < 0) {
+            perror("ERROR: could not write to server");
+            exit(1);
+        }
+        totalsent += bytes_sent;
+        count -= bytes_sent;
+    }
+
+    return totalsent;
+}
+
+/*
+* read from socket fd iterativly and returns the number of bytes read
+*
+*/
+int read_data_from_server(int sockfd, char* buffer, int count) {
+    int bytes_read = 1;
+    int totalread = 0;
+    
+    while (count > 0) {  /* C is 32bits == 4bytes*/
+        bytes_read = read(sockfd, buffer+totalread, count);
+        if (bytes_read < 0) {
+            perror("ERROR: an error accured while reading bytes from server");
+            exit(1);
+        }
+        totalread += bytes_read;
+        count -= bytes_read;
+    }
+
+    return totalread;
+}
+
---
+++ pcc_server.c
@@ -0,0 +1,294 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#define MAX_BATCH_SIZE 1048576
+#define min(X, Y) (((X) < (Y)) ? (X) : (Y))
+
+/* prototypes */
+void define_sigint();
+void sigint_handler();
+int read_data_from_client(int connfd, char* buffer, int count);
+int send_data_to_client(int connfd, char* buffer, int count);
+void terminate_server();
+
+/* global variables */
+uint32_t pcc_total[95]; /* a data structure pcc_total that will count how many times each printable character was observed */
+int client_in_process = 0; /* flag to indicate if there is a client the server processes */
+int sigint = 0; /* flag to indicate if SIGINT was activated */
+
+
+int main(int argc, char const *argv[]) {
+    int server_port;
+    struct sockaddr_in serv_addr;
+    uint32_t N;
+    uint32_t endian_N;
+    int bytes_left;
+    int batch_size;
+    char* batch_buffer;
+    uint32_t curr_pcc_count[95];
+    int i;
+    uint32_t C;
+    uint32_t endian_C;
+    socklen_t addrsize = sizeof(struct sockaddr_in);
+    int listenfd = -1;
+    int connfd = -1;
+    int totalread = 0;
+    int totalsent = 0;
+    int read_error_accured = 0;
+
+    define_sigint();  /* SIGINT handler*/
+
+    if (argc != 2) {
+        perror("ERROR: invalid number of arguments");
+        exit(1);
+    }
+
+    /* get cmd arguments */
+    server_port = atoi(argv[1]);
+
+    /* Listen to incoming TCP connections on the specified server port (code is mainly based on rec10) */
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("ERROR: an error accured while creating a socket");
+        exit(1);
+    }
+    memset(&serv_addr, 0, addrsize);
+
+    serv_addr.sin_family = AF_INET;
+    /* INADDR_ANY = any local machine address */
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(server_port);
+
+    /* enable reusing the port quickly after the server terminate
+    helped here: https://stackoverflow.com/questions/24194961/how-do-i-use-setsockoptso-reuseaddr */
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
+        perror("ERROR: an error accured while setsockopt");
+        exit(1);
+    }
+
+    /* bind */
+    if (0 != bind(listenfd,
+                 (struct sockaddr*) &serv_addr,
+                 addrsize)) {
+        perror("ERROR: Bind Failed");
+        exit(1);
+    }
+
+    /* listen - queue of size 10 */
+    if (0 != listen(listenfd, 10)) {
+        perror("ERROR: Listen Failed");
+        exit(1);
+    }
+
+    /* iterativly - accepts a TCP connection */
+    while (1) {
+        /* if SIGINT was sent - don't accept another connection and terminate server */
+        if (sigint) {
+            terminate_server();
+        }
+
+        /* accept */
+        connfd = accept(listenfd,
+                        NULL,
+                        NULL);
+        if (connfd < 0) {
+            perror("ERROR: Accept Failed");
+            exit(1);
+        }
+
+        client_in_process = 1; /* mark that a client is connected */
+
+        totalread = read_data_from_client(connfd, (char*) &endian_N, 4); /*32bits == 4 bytes */
+
+        /* in case the reading error is one of ETIMEDOUT, ECONNRESET, or EPIPE 
+        * OR the client was killed unexpectedly - start new client connection */
+        if (totalread < 0 || totalread != 4) {
+            if (totalread != 4) {
+                perror("ERROR: Client connections may have terminated due to client process was killed unexpectedly");
+            }
+            close(connfd);
+            connfd = -1;
+            client_in_process = 0;
+            continue;
+        }
+        
+        /* get N */
+        N = ntohl(endian_N);
+
+        /* recieve client data in batches and calculate pcc */            
+        bytes_left = N;
+        totalread = 0;
+        C = 0;
+        while (bytes_left > 0) {
+            /* create a buffer of the maximal amount of data recieved */
+            batch_size = min(bytes_left, MAX_BATCH_SIZE);
+
+            batch_buffer = (char*) malloc(batch_size*sizeof(char));
+            if (batch_buffer == NULL) {
+                perror("ERROR: an error accured while allocating memory to batch buffer");
+                exit(1);
+            }
+            
+            totalread = read_data_from_client(connfd, batch_buffer, batch_size);
+
+            /* in case the reading error is one of ETIMEDOUT, ECONNRESET, or EPIPE OR the client was killed unexpectedly - start new client connection */
+            if (totalread < 0 || totalread != batch_size) {
+                if (totalread != batch_size) {
+                    perror("ERROR: Client connections may have terminated due to client process was killed unexpectedly");
+                }
+                close(connfd);
+                read_error_accured = 1;
+                connfd = -1;
+                client_in_process = 0;
+                break;
+            }
+
+            /* count pcc for current batch */
+            for (i = 0; i < batch_size; i++) {
+                if (32 <= batch_buffer[i] && batch_buffer[i] <= 126) {
+                    C++;
+                    curr_pcc_count[batch_buffer[i] - 32]++;
+                }
+            }
+            free(batch_buffer);
+
+            bytes_left -= batch_size;
+        }
+        /* in case the batch reader while loop break due to reading error - continue to next client connection */
+        if (read_error_accured) {
+            continue;
+        }
+
+        /* send C back to client */
+        endian_C = htonl(C);
+        totalsent = send_data_to_client(connfd, (char*) &endian_C, 4); /*32bits == 4bytes*/
+
+        /* in case the sending error is one of ETIMEDOUT, ECONNRESET, or EPIPE OR the client was killed unexpectedly - start new client connection */
+        if (totalsent < 0 || totalsent != 4) {
+            if (totalsent != 4) {
+                perror("ERROR: Client connections may have terminated due to client process was killed unexpectedly");
+            }
+            close(connfd);
+            connfd = -1;
+            client_in_process = 0;
+            continue;
+        }
+
+        /* After sending the result to the client, update the pcc_total global data structure */
+        for (i = 0; i < 95; i++){
+            pcc_total[i] = curr_pcc_count[i];
+        }
+
+        close(connfd);
+        client_in_process = 0;
+    }
+    return 0;
+}
+
+
+/*
+* A function to handle SIGINT signal with SIG_IGN
+* Ignored signal behavior
+* helped here - recitation3 signal_handler.c code
+*/
+void define_sigint() {
+    struct sigaction new_action = {
+        .sa_handler = sigint_handler,
+        .sa_flags = SA_RESTART,
+    };  
+    
+    if (sigaction(SIGINT, &new_action, NULL) == -1) {
+        perror("ERROR: Failed executing SIGINT sigaction");
+        exit(1);
+    }
+}
+
+void sigint_handler() {
+    if (client_in_process) {
+        sigint = 1;
+    } else {
+        terminate_server();
+    }
+}
+
+/*
+* read from connection fd iterativly and returns the number of bytes read
+*
+*/
+int read_data_from_client(int connfd, char* buffer, int count) {
+    int bytes_read = 1;
+    int totalread = 0;
+    
+    while (count > 0) {
+        bytes_read = read(connfd, buffer+totalread, count);
+        
+        if (bytes_read < 0) {
+            totalread = -1;
+            break;
+        }
+
+        totalread += bytes_read;
+        count -= bytes_read;
+    }
+
+    /* check for errors */
+    if (bytes_read < 0) {
+        if (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE) {
+            perror("ERROR: an error accured while reading from client, closing current client connection");
+        } else {
+            perror("ERROR: an error accured while reading from client, exit server");
+            exit(1);
+        }
+    }
+    return totalread;
+}
+
+/*
+* write to connection fd iterativly and returns the number of bytes written
+*
+*/
+int send_data_to_client(int connfd, char* buffer, int count) {
+    int bytes_sent;
+    int totalsent = 0;
+
+    /* iterate until all buffer bytes are written to connection fd */
+    while (count > 0) {
+        bytes_sent = write(connfd, buffer+totalsent, count);
+
+        if (bytes_sent < 0) {
+            totalsent = -1;
+            break;
+        }
+        totalsent += bytes_sent;
+        count -= bytes_sent;
+    }
+
+    /* check for errors */
+    if (bytes_sent < 0) {
+        if (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE) {
+            perror("ERROR: an error accured while reading from client, closing current client connection");
+        } else {
+            perror("ERROR: an error accured while reading from client, exit server");
+            exit(1);
+        }
+    }
+
+    return totalsent;
+}
+
+/*
+* For every printable character, print the number of times it was observed 
+*/
+void terminate_server() {
+    int i = 0;
+
+    for (i = 0; i < 95; i++){
+        printf("char '%c' : %u times\n", i+32, pcc_total[i]);
+    }
+    exit(0);
+}
